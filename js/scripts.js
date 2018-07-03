625f7c0d736d5aa830a269ffca25eb4908842eea
Ryeker <ryekerjh@gmail.com>
2018-07-02 20:36:59 -0700

    finished day 1 in class work

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//DECLARE A VARIABLE
+var firstName;
+
+//DEFINE YOUR VARIABLE
+firstName = "Ryeker"; //String
+
+//DEFINE AND DECLARE YOUR VARIABLE
+var lastName = "Herndon";
+
+console.log(firstName);
+
+//SINGLE LINE COMMENT
+
+/* MULTI LINE COMMENT
+firstName = 'Superman';
+console.log(firstName);
+*/
+
+var getTweetDataAsync; //This is the variable that will fetch teh tweets
+
+{
+  //New Scope. 
+  var myName;
+}
+
+
+
+
+
+
+
+
+
+//
