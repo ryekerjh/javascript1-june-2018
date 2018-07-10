fbbc168cbe615eb71f866349f5f9cf34679b9944
Ryeker <ryekerjh@gmail.com>
2018-07-09 21:16:19 -0700

    adding day 2 deatils

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,33 @@
 var myString;
 var myNumber;
-var myOtherNumber;
-
+var myOtherNumber; //VARIABLE declarations
 
 var myString = "Hello World!*76 ";
 
-var myNumber = 43; //PRIMITIVE
-var myOtherNumber = new Number(43); //NUMBER OBJECT
+var myNumber = 43; //PRIMITIVE number type
+var myOtherNumber = new Number(43); //NUMBER OBJECT type
 
 var isLoggedIn = false; //Boolean
 
 let newVariable = "I am the best variable!";
-// console.log(newVariable, 'AFTER being declared');
+console.log(newVariable, 'AFTER being declared');
 
 {
-  let newVariable = "I am the worst...";
-  // console.log(newVariable, 'INSIDE the scope');
+  let newVariable = "I am the worst..."; //This will work because the let is INSIDE the scope created by this block {}
+  console.log(newVariable, 'INSIDE the scope');
 }
-// let newVarible; //DECLARATION
-newVariable = "My dog is ugly.";
-// console.log(newVariable, 'OUTSIDE the scope');
 
-isLoggedIn = null;
+let newVarible; //This will throw an error because let variables can't be redeclared
+newVariable = "My dog is ugly."; //reassignment. This is legal.
+console.log(newVariable, 'OUTSIDE the scope');
 
 
 //STRING interpolation
-// console.log("Hey there, " + myString + " how are you?"); //ES5
-// console.log(`Hey there, ${myString} how are you?`); //ES6
+console.log("Hey there, " + myString + " how are you?"); //ES5
+console.log(`Hey there, ${myString} how are you?`); //ES6
 
-var dozen = 12;
+
+let dozen = 12; // if this was a var, it would reassign this value everywhere.
 {
   dozen = 13;
   //BLOCK OF CODE
@@ -36,36 +35,38 @@ var dozen = 12;
 
 const age = 32;
 {
-  const age = 35;
+  const age = 35; //This will be reassigned but only in this scope
   console.log(age);
 };
 
 
-// console.log(age);
+console.log(age); //Will still be 32
 
-let today = new Date();
-let myBirthday = new Date('5.21.1986');
-// console.log(myBirthday);
+let today = new Date(); //Will make a date object for RIGHT NOW
+let myBirthday = new Date('5.21.1986'); //Will make a date object for the given date string
+console.log(myBirthday);
 
 let bestNumber = 42.7;
 
-Math.floor(bestNumber);
-let calc = Math.ceil(bestNumber) * Math.PI;
-// console.log(Math.ceil(calc));
+Math.floor(bestNumber); //Rounds down to the nearest WHOLE number
+Math.ceil(bestNumber); //Rounds up to the nearest WHOLE number
+
+let calc = Math.ceil(bestNumber) * Math.PI; //multiplication!!
+console.log(Math.ceil(calc));
 
 
 let classRoom = "Room 1";
 let objectClassRoom = new String("Room 1");
-// console.log(typeof classRoom);
-// console.log(typeof objectClassRoom);
-// console.log(classRoom.length);
+console.log(typeof classRoom);
+console.log(typeof objectClassRoom);
+console.log(classRoom.length); //Will give you count of characters in the string
 
-// alert("Hey there, user! Pick a path...");//1-directional comm to your user
-// const isReady = confirm("Are you ready to party?");
+alert("Hey there, user! Pick a path...");//1-directional comm to your user
+const isReady = confirm("Are you ready to party?"); //Returns a boolean
 
-// alert(isReady);
+alert(isReady);
 
-let userName = prompt("What's your name?", "John");
+let userName = prompt("What's your name?", "John"); //Returns a full string from your user
 console.log(userName);
 
 
