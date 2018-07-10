b9feec3dbd56cce4a5d2eaa76cdfa674ba6d193d
Ryeker <ryekerjh@gmail.com>
2018-07-09 21:03:39 -0700

    cleaning up day 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 //DECLARE A VARIABLE
 var firstName;
-
+//At this point, firstName is undefined
+ 
 //DEFINE YOUR VARIABLE
-firstName = "Ryeker"; //String
+firstName = "Ryeker"; //String = any characters in quotations (single or double, as long as they match!)
 
 //DEFINE AND DECLARE YOUR VARIABLE
 var lastName = "Herndon";
 
-console.log(firstName);
+console.log(firstName); //Prints the value to your console.
 
 //SINGLE LINE COMMENT
 
@@ -18,17 +19,7 @@ console.log(firstName);
 
 var getTweetDataAsync; //This is the variable that will fetch teh tweets
 
-{
-  //New Scope. 
+{ //These braces create a BLOCK of code
+  //New Scope is created inside each block. 
   var myName;
 }
-
-
-
-
-
-
-
-
-
-//
