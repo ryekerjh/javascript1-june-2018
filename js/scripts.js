e2eb23734fded45f47bd29501919f225f7402350
Ryeker <ryekerjh@gmail.com>
2018-07-18 18:31:58 -0700

    finished methods and functions

--- js/scripts.js
+++ js/scripts.js
@@ -51,10 +51,10 @@ function iCallMyself() {
 //FAT ARROW FUNCTIONS
 
 //BASIC IMPPLEMENTATION
-(string, number) => { //NO FUNCTION KEYWORD
-  console.log(string);
-  alert(number);
-};
+// (string, number) => { //NO FUNCTION KEYWORD
+//   console.log(string);
+//   alert(number);
+// };
 
 //ONLY 1 ARGUMENTS
 string => { //PARENS ARE OPTIONAL
@@ -70,6 +70,49 @@ let logString = string => {
 //IMPLICIT RETURN
 string => (string.toUpperCase());
 string => string.toUpperCase();
+
+
+//TRY IT OUT #1
+/*
+Accept 2 arguments (age of person now, amount of product per day).
+Calculate the supply needed for the rest of the user's life, based on a constant max age of 80.
+Alert the answer in this format: "You will need x to last you until you're y.
+BONUS: write it to accept a dynamic max age.
+*/
+function calcLifetimeSupply(currentAge, dailyUse, maxAge) {
+  if(isNaN(currentAge) && isNaN(dailyUse) && isNaN(maxAge)) {
+    alert("Please give me a number!");
+    let currentAgeSecondTry = prompt("Whats your age?");
+    let dailyUseSecondTry = prompt("How many per year?");
+    let maxAgeSecondTry = prompt("You gonna die doe?");
+    calcLifetimeSupply(currentAgeSecondTry, dailyUseSecondTry, maxAgeSecondTry);
+  } else {
+    if(curentAge < maxAge) {
+      let supply = (maxAge - currentAge) * (dailyUse * 365);
+      alert(`You will need ${supply} to last until you're ${maxAge}`);
+    } else {
+      alert("this is...awkward...youre dead.");
+    }
+  }
+}
+
+// let age = prompt('...howold?');
+// ...
+// calcLifetimeSupply( )
+// function callCalculator() {
+//   prompt()
+// }
+
+//METHODS
+console.log(3444343);//log was written to consume args
+"cheese".toUpperCase();//toUpperCase was written to consume 0 arguments
+
+
+
+
+
+
+
 
 
 //
