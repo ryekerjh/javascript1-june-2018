ba9f7060eb18610da347d16ed9316d6a08574e5f
Ryeker <ryekerjh@gmail.com>
2018-07-23 19:00:44 -0700

    adding takehome work

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
-//QUERYING THE DOM
-const body = document.getElementsByTagName("body");
-// console.log(body[0]);
-const header = document.getElementsByClassName("heading");
-
-const paragraph = document.getElementById("paragraph1");
-console.log(paragraph);
-
-let div = document.querySelector("#beth_is_the_worst");
-
-div.addEventListener(
-  "mouseover",
-  ev => { //ES6 way to write callback
-    console.log(ev);
-    div.style.backgroundColor = "indigo";
+/*
+1. Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. So if they give you 90 Fahrenheit, they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
+*/
+const h3 = document.querySelector('#temp');
+let scale = prompt("What scale (C or F) do you want to use?", "F or C");
+let temp = prompt("How hot is it?", "99");
+/*
+// 2. Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
+*/
+function convertTemp(scale, temp) {
+  scale = scale.toLowerCase();
+  let returnVal;
+  let oppositeVal = scale === 'c' ? 'Fahrenheit' : 'Celcius';
+  temp = parseInt(temp);
+  if(!isNaN(temp)) {
+    returnVal = scale === 'c' ? (1.8 * temp) + 32 : (5 / 9) * (temp - 32);
+    /*
+    3. Display the temperature in an h3 with minimal styling.
+    */
+    h3.innerText = `Your temperature is ${returnVal} ${oppositeVal}`;
+  } else {
+    scale = prompt("What scale (C or F) do you want to use?", "F or C");
+    temp = prompt("How hot is it?", "99");
+    convertTemp(scale, temp);
   }
-  
-  // function(ev) { //ES5 way to write callback
-  //   div.style.backgroundColor = "indigo";
-  // }
-);
+}
+
+convertTemp(scale, temp);
+
+/*
+// 4. From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
+// Display the converted temperature in the h3 each time it is changed.
+*/
+h3.addEventListener('click', (e) => {
+  let valueArr = e.target.innerText.split(' ');
+  scale = 
+  valueArr[valueArr.length - 1] === 'Celcius' ? 'C' : 'F';
+  temp = valueArr[valueArr.length - 2];
+  convertTemp(scale, temp);
+});
